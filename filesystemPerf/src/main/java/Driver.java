af714272ece0f31d44494e13ebf58a70c163f2df
Maithem <Maithem@users.noreply.github.com>
2019-01-27 23:24:15 -0800

    Added different fsync configurations
    SSD Stats
    desc: oneShotFsync 10mb duration(ms): 60.413205 ret: 10000000
    desc: oneShotFsync 20mb duration(ms): 88.35912 ret: 20000000
    desc: oneShotFsync 30mb duration(ms): 121.81471 ret: 30000000
    desc: oneShotFsync 50mb duration(ms): 199.105707 ret: 50000000
    desc: oneShotFsync 100mb duration(ms): 377.345746 ret: 100000000
    desc: oneShotNoFsync 10mb duration(ms): 16.437455 ret: 10000000
    desc: oneShotNoFsync 50mb duration(ms): 59.074626 ret: 50000000
    desc: oneShotNoFsync 100mb duration(ms): 117.713933 ret: 100000000
    desc: chunkedFsync 100mb/10mb duration(ms): 776.640591 ret: 100000000
    desc: multiFileWriteFsyncPerWrite 100mb/10mb/3files duration(ms): 466.91415 ret: 100000000
    desc: multiFileWritePostFsync 100mb/10mb/3files duration(ms): 347.367982 ret: 100000000
    desc: multiFileWriteFsyncPerWrite 500mb/10mb/3files duration(ms): 2348.3085 ret: 500000000
    desc: multiFileWritePostFsync 500mb/10mb/3files duration(ms): 1607.408554 ret: 500000000
    
    7400rpm 1Tb harddrive stats
    
    desc: oneShotFsync 10mb duration(ms): 171.15088 ret: 10000000
    desc: oneShotFsync 20mb duration(ms): 185.252189 ret: 20000000
    desc: oneShotFsync 30mb duration(ms): 248.760744 ret: 30000000
    desc: oneShotFsync 50mb duration(ms): 382.197766 ret: 50000000
    desc: oneShotFsync 100mb duration(ms): 707.107417 ret: 100000000
    desc: oneShotNoFsync 10mb duration(ms): 14.900898 ret: 10000000
    desc: oneShotNoFsync 50mb duration(ms): 54.246292 ret: 50000000
    desc: oneShotNoFsync 100mb duration(ms): 108.864822 ret: 100000000
    desc: chunkedFsync 100mb/10mb duration(ms): 1978.213682 ret: 100000000
    desc: multiFileWriteFsyncPerWrite 100mb/10mb/3files duration(ms): 1224.455086 ret: 100000000
    desc: multiFileWritePostFsync 100mb/10mb/3files duration(ms): 774.038975 ret: 100000000
    desc: multiFileWriteFsyncPerWrite 500mb/10mb/3files duration(ms): 6273.579121 ret: 500000000
    desc: multiFileWritePostFsync 500mb/10mb/3files duration(ms): 3257.488218 ret: 500000000

--- filesystemPerf/src/main/java/Driver.java
+++ filesystemPerf/src/main/java/Driver.java
@@ -8,19 +8,37 @@ public class Driver {
         String path = "/tmp/data";
         FileManager fileManager = new FileManager(path);
         FileChannel channel = fileManager.getNewFile();
+        FileChannel channel2 = fileManager.getNewFile();
+        FileChannel channel3 = fileManager.getNewFile();
+        FileChannel channel4 = fileManager.getNewFile();
+
         IGenerator generator = new Generator();
 
         ByteBuffer buf10mb = generator.generateDataInMB(10);
+        ByteBuffer buf20mb = generator.generateDataInMB(20);
+        ByteBuffer buf30mb = generator.generateDataInMB(30);
         ByteBuffer buf50mb = generator.generateDataInMB(50);
         ByteBuffer buf100mb = generator.generateDataInMB(100);
+        ByteBuffer buf500mb = generator.generateDataInMB(500);
 
         time(() -> WritingMethod.oneShotFsync(buf10mb.slice(), channel), "oneShotFsync 10mb");
+        time(() -> WritingMethod.oneShotFsync(buf20mb.slice(), channel), "oneShotFsync 20mb");
+        time(() -> WritingMethod.oneShotFsync(buf30mb.slice(), channel), "oneShotFsync 30mb");
         time(() -> WritingMethod.oneShotFsync(buf50mb.slice(), channel), "oneShotFsync 50mb");
         time(() -> WritingMethod.oneShotFsync(buf100mb.slice(), channel), "oneShotFsync 100mb");
 
         time(() -> WritingMethod.oneShotNoFsync(buf10mb.slice(), channel), "oneShotNoFsync 10mb");
         time(() -> WritingMethod.oneShotNoFsync(buf50mb.slice(), channel), "oneShotNoFsync 50mb");
         time(() -> WritingMethod.oneShotNoFsync(buf100mb.slice(), channel), "oneShotNoFsync 100mb");
+
+        time(() -> WritingMethod.chunkedFsync(buf100mb.slice(), 10_000_000, channel), "chunkedFsync 100mb/10mb");
+
+        FileChannel[] channels = {channel2, channel3, channel4};
+        time(() -> WritingMethod.multiFileWriteFsyncPerWrite(buf100mb.slice(), 10_000_000, channels), "multiFileWriteFsyncPerWrite 100mb/10mb/3files");
+        time(() -> WritingMethod.multiFileWritePostFsync(buf100mb.slice(), 10_000_000, channels), "multiFileWritePostFsync 100mb/10mb/3files");
+
+        time(() -> WritingMethod.multiFileWriteFsyncPerWrite(buf500mb.slice(), 10_000_000, channels), "multiFileWriteFsyncPerWrite 500mb/10mb/3files");
+        time(() -> WritingMethod.multiFileWritePostFsync(buf500mb.slice(), 10_000_000, channels), "multiFileWritePostFsync 500mb/10mb/3files");
 
         fileManager.close();
     }
--- filesystemPerf/src/main/java/WritingMethod.java
+++ filesystemPerf/src/main/java/WritingMethod.java
@@ -14,4 +14,59 @@ public class WritingMethod {
         channel.force(true);
         return numBytes;
     }
+
+    public static int chunkedFsync(ByteBuffer buf, int chunkSize, FileChannel channel) throws IOException {
+        int numBytes = 0;
+
+        while(buf.hasRemaining()) {
+            ByteBuffer sliced = buf.slice();
+            int stride = Math.min(sliced.remaining(), chunkSize);
+            sliced.limit(stride);
+            numBytes += channel.write(sliced);
+            channel.force(true);
+            buf.position(buf.position() + stride);
+        }
+
+        return numBytes;
+    }
+
+    public static int multiFileWriteFsyncPerWrite(ByteBuffer buf, int chunkSize, FileChannel[] channels) throws IOException {
+        int numBytes = 0;
+
+        int fileIdx = 0;
+
+        while(buf.hasRemaining()) {
+            ByteBuffer sliced = buf.slice();
+            int stride = Math.min(sliced.remaining(), chunkSize);
+            sliced.limit(stride);
+            numBytes += channels[fileIdx % channels.length].write(sliced);
+            channels[fileIdx % channels.length].force(true);
+            buf.position(buf.position() + stride);
+            fileIdx++;
+        }
+
+        return numBytes;
+    }
+
+    public static int multiFileWritePostFsync(ByteBuffer buf, int chunkSize, FileChannel[] channels) throws IOException {
+        int numBytes = 0;
+
+        int fileIdx = 0;
+
+        while(buf.hasRemaining()) {
+            ByteBuffer sliced = buf.slice();
+            int stride = Math.min(sliced.remaining(), chunkSize);
+            sliced.limit(stride);
+            numBytes += channels[fileIdx % channels.length].write(sliced);
+            //channels[fileIdx % channels.length].force(true);
+            buf.position(buf.position() + stride);
+            fileIdx++;
+        }
+
+        for (FileChannel channel : channels) {
+            channel.force(true);
+        }
+
+        return numBytes;
+    }
 }
